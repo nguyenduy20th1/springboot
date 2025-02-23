c919ee1156e88ce895113bf4bf91f1e46d3091d0
nguyenduy <ngocduy2003.vd@gmail.com>
2025-02-23 23:49:32 +0700

    update

---
+++ lab02/src/main/java/com/example/lab02/phan3/b10_ktraMau.java
@@ -0,0 +1,25 @@
+package com.example.lab02.phan3;
+
+import java.util.Scanner;
+
+public class b9_ktraMau {
+    public static void main(String[] args) {
+        char color;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập kí tự màu(r, b, g, p): ");
+        color = sc.next().charAt(0);
+        switch (color) {
+            case 'r', 'R':
+                System.out.println("RED");
+                break;
+            case 'b', 'B':
+                System.out.println("BLUE");
+                break;
+            case 'g', 'G':
+                System.out.println("GREEN");
+                break;
+            case 'p', 'P':
+                System.out.println("YELLOW");
+        }
+    }
+}
---
+++ lab02/src/main/java/com/example/lab02/phan3/b7_soNLonNhat.java
@@ -0,0 +1,4 @@
+package com.example.lab02.phan3;
+
+public class b7_soNLonNhat {
+}
